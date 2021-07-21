e42a2f1998157b07ae78591637ee2a037dda32cb
yanisyoung <yanisyoung@163.com>
2021-07-21 21:26:23 +0800

    up 加载yml属性到class测试

--- src/main/java/com/learn/test/demo/config/ConfigModelDemo.java
+++ src/main/java/com/learn/test/demo/config/ConfigModelDemo.java
@@ -1,5 +1,6 @@
 package com.learn.test.demo.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Component;
  * @author Bai
  * @date 2021/7/19 20:36
  */
+@Data
 //将这个类注入spring容器
 @Component
 //将属性注入这个类
 @ConfigurationProperties(prefix = "configdemo")
 public class ConfigModelDemo {
 	private UserModel user;
+
+	@Override
+	public String toString () {
+		return "ConfigModelDemo{" + "user=" + user + '}';
+	}
 }
--- src/main/java/com/learn/test/demo/config/UserModel.java
+++ src/main/java/com/learn/test/demo/config/UserModel.java
@@ -10,14 +10,62 @@ import lombok.ToString;
  * @author Bai
  * @date 2021/7/19 20:37
  */
-@Data
 @ToString
 public class UserModel {
 	private String name;
 	private Long age;
 	private List<String> hobbies;
-	private Map<String, Long> clazz;
+	private Map<String, String> clazz;
+	private Map<String, String> books;
 	private Dog dog;
+
+	public String getName () {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public Long getAge () {
+		return age;
+	}
+
+	public void setAge (Long age) {
+		this.age = age;
+	}
+
+	public List<String> getHobbies () {
+		return hobbies;
+	}
+
+	public void setHobbies (List<String> hobbies) {
+		this.hobbies = hobbies;
+	}
+
+	public Map<String, String> getClazz () {
+		return clazz;
+	}
+
+	public void setClazz (Map<String, String> clazz) {
+		this.clazz = clazz;
+	}
+
+	public Map<String, String> getBooks () {
+		return books;
+	}
+
+	public void setBooks (Map<String, String> books) {
+		this.books = books;
+	}
+
+	public Dog getDog () {
+		return dog;
+	}
+
+	public void setDog (Dog dog) {
+		this.dog = dog;
+	}
 }
 
 @Data
